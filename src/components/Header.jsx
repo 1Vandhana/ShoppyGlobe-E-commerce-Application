8f68871bd0a309b7be70be855386b42aae83960d
vandhanad779-glitch <21951a67h0@iare.ac.in>
2026-02-19 01:41:36 +0530

    search bar icon added by installing the package

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { setSearch } from "../redux/searchSlice";
+import { FiSearch } from "react-icons/fi";
+
 
 function Header() {
   const dispatch = useDispatch();
@@ -13,18 +15,20 @@ function Header() {
 
   return (
     <header className="header">
-      <h2> 🛒 ShoppyGlobe</h2>
+      <h2> 🛍️ ShoppyGlobe</h2>
 
       <nav>
         <Link to="/">Home</Link>
         <Link to="/cart">Cart ({totalCount})</Link>
       </nav>
-
-      <input
-        type="text"
-        placeholder="Search products..."
-        onChange={(e) => dispatch(setSearch(e.target.value))}
-      />
+      <div className="search-wrapper">
+        <span className="search-icon">
+            <FiSearch />
+        </span>
+      
+        <input type="text" placeholder="Search..." 
+        onChange={(e) => dispatch(setSearch(e.target.value))}/>
+      </div>
     </header>
   );
 }
